14afc9e7ee5864901874ff77a5471e7ad003c741
aws06 <aws06@CESAEPRT.WAN>
2025-04-14 15:44:06 +0100

    Enable versioning for the bucket. This allows you to retrieve all state versions

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tfstate" {
-  bucket_prefix = "terraform-state
+  bucket_prefix = "terraform-state"
 
   tags = {
     Owner = "Platform Team"
@@ -8,5 +8,13 @@ resource "aws_s3_bucket" "tfstate" {
 
   lifecycle {
     prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate"{
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
